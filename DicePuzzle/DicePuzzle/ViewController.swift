626dbc672be36c40e26d42f81d600ee508e05bf7
Marek Niedbach <marekniedbach@MacBook-Pro-Marek.local>
2017-09-03 12:14:54 +0200

    Initial commit

---
+++ DicePuzzle/DicePuzzle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DicePuzzle
+//
+//  Created by Marek Niedbach on 03.09.2017.
+//  Copyright © 2017 Marek Niedbach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,21 @@
+workspace 'DicePuzzle'
+project 'DicePuzzle/DicePuzzle.xcodeproj'
+platform :ios, '10.0'
+
+target 'DicePuzzle' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for DicePuzzle
+
+  target 'DicePuzzleTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'DicePuzzleUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
