3326382ba59ef1cb97390b0a9176f715c49daa78
Marek Niedbach <m.niedbach@gmail.com>
2018-12-06 17:48:10 +0100

    Board Swapper

---
+++ DicePuzzle/DicePuzzle/Domain/BoardSwapper.swift
@@ -0,0 +1,93 @@
+//
+//  Created by Marek Niedbach on 06/12/2018.
+//  Copyright © 2018 Marek Niedbach. All rights reserved.
+//
+
+import Foundation
+
+public protocol SwappableBoard {
+    var size: Int { get }
+    func swap(rowAt row: Int, direction: DiceSwapDirection)
+    func swap(colAt col: Int, direction: DiceSwapDirection)
+    func swap(diagonal: Diagonal, direction: DiceSwapDirection)
+}
+
+public class BoardSwapper {
+    private let board: SwappableBoard
+    private var beginPosition = DicePosition(row: 0, col: 0)
+    private var endPosition = DicePosition(row: 0, col: 0)
+    private var rowIndex: Int { return beginPosition.row }
+    private var colIndex: Int { return beginPosition.col }
+    private var swappedOnDexter: Bool {
+        return isOnDexter(beginPosition) && isOnDexter(endPosition)
+    }
+    private var swappedOnSinister: Bool {
+        return isOnSinister(beginPosition) && isOnSinister(endPosition)
+    }
+
+    public init(board: SwappableBoard) {
+        self.board = board
+    }
+
+    public func begin(at position: DicePosition) {
+        beginPosition = position
+    }
+
+    public func end(at position: DicePosition) {
+        endPosition = position
+        detectSwap()
+    }
+
+    private func detectSwap() {
+        switch positionChange() {
+        case (rowChange: .unchanged, colChange: .incremented):
+            board.swap(rowAt: rowIndex, direction: .forward)
+        case (rowChange: .unchanged, colChange: .decremented):
+            board.swap(rowAt: rowIndex, direction: .backward)
+        case (rowChange: .incremented, colChange: .unchanged):
+            board.swap(colAt: colIndex, direction: .forward)
+        case (rowChange: .decremented, colChange: .unchanged):
+            board.swap(colAt: colIndex, direction: .backward)
+        case (rowChange: .incremented, colChange: .incremented) where swappedOnDexter:
+            board.swap(diagonal: .dexter, direction: .forward)
+        case (rowChange: .decremented, colChange: .decremented) where swappedOnDexter:
+            board.swap(diagonal: .dexter, direction: .backward)
+        case (rowChange: .incremented, colChange: .decremented) where swappedOnSinister:
+            board.swap(diagonal: .sinister, direction: .forward)
+        case (rowChange: .decremented, colChange: .incremented) where swappedOnSinister:
+            board.swap(diagonal: .sinister, direction: .backward)
+        default:
+            break
+        }
+    }
+
+    private func isOnDexter(_ position: DicePosition) -> Bool {
+        return position.row == position.col
+    }
+
+    private func isOnSinister(_ position: DicePosition) -> Bool {
+        return position.row == board.size - position.col - 1
+    }
+
+    private func positionChange() -> (rowChange: Change, colChange: Change) {
+        let rowChange = Change.detected(from: beginPosition.row, to: endPosition.row)
+        let colChange = Change.detected(from: beginPosition.col, to: endPosition.col)
+        return (rowChange: rowChange, colChange: colChange)
+    }
+
+    private enum Change {
+        case incremented
+        case decremented
+        case unchanged
+
+        static func detected(from begin: Int, to end: Int) -> Change {
+            if begin < end {
+                return .incremented
+            } else if begin > end {
+                return .decremented
+            } else {
+                return .unchanged
+            }
+        }
+    }
+}
--- DicePuzzle/DicePuzzleTests/Domain/BoardSwapperTests.swift
+++ DicePuzzle/DicePuzzleTests/Domain/BoardSwapperTests.swift
@@ -9,44 +9,6 @@
 import XCTest
 @testable import DicePuzzle
 
-
-protocol SwappableBoard {
-    func swap(rowAt row: Int, direction: DiceSwapDirection)
-}
-
-class DiceBoardMock: SwappableBoard {
-    var swappedRow: Int?
-    var swappedDirection: DiceSwapDirection?
-
-    func swap(rowAt row: Int, direction: DiceSwapDirection) {
-        swappedRow = row
-        swappedDirection = direction
-    }
-}
-
-class BoardSwapper {
-    private let board: SwappableBoard
-    private var beginPosition = DicePosition(row: 0, col: 0)
-
-    init(board: SwappableBoard) {
-        self.board = board
-    }
-
-    func begin(at position: DicePosition) {
-        beginPosition = position
-    }
-
-    func end(at position: DicePosition) {
-        if position.row == beginPosition.row {
-            if position.col > beginPosition.col {
-                board.swap(rowAt: position.row, direction: .forward)
-            } else if position.col < beginPosition.col {
-                board.swap(rowAt: position.row, direction: .backward)
-            }
-        }
-    }
-}
-
 class BoardSwapperTests: XCTestCase {
     private let board = DiceBoardMock()
     private lazy var sut = BoardSwapper(board: board)
@@ -67,24 +29,102 @@ class BoardSwapperTests: XCTestCase {
         XCTAssertEqual(board.swappedDirection, .backward)
     }
 
-    func testDontSwapRowIfPositionNotChanged() {
+    func testSwapColumnForward() {
+        sut.begin(at: DicePosition(row: 1, col: 1))
+        sut.end(at: DicePosition(row: 4, col: 1))
+
+        XCTAssertEqual(board.swappedCol, 1)
+        XCTAssertEqual(board.swappedDirection, .forward)
+    }
+
+    func testSwapColumnBackward() {
+        sut.begin(at: DicePosition(row: 3, col: 3))
+        sut.end(at: DicePosition(row: 2, col: 3))
+
+        XCTAssertEqual(board.swappedCol, 3)
+        XCTAssertEqual(board.swappedDirection, .backward)
+    }
+
+    func testDontSwapIfPositionNotChanged() {
         sut.begin(at: DicePosition(row: 3, col: 1))
         sut.end(at: DicePosition(row: 3, col: 1))
 
-        XCTAssertNil(board.swappedRow)
+        XCTAssertNil(board.swappedDirection)
     }
 
-    func testDontSwapRowForwardIfRowChanged() {
+    func testSwapDexterForward() {
+        sut.begin(at: DicePosition(row: 1, col: 1))
+        sut.end(at: DicePosition(row: 3, col: 3))
+
+        XCTAssertEqual(board.swappedDiagonal, .dexter)
+        XCTAssertEqual(board.swappedDirection, .forward)
+    }
+
+    func testSwapDexterBackward() {
+        sut.begin(at: DicePosition(row: 4, col: 4))
+        sut.end(at: DicePosition(row: 3, col: 3))
+
+        XCTAssertEqual(board.swappedDiagonal, .dexter)
+        XCTAssertEqual(board.swappedDirection, .backward)
+    }
+
+    func testDontSwapDexterIfNotOnDiagonal() {
+        sut.begin(at: DicePosition(row: 1, col: 2))
+        sut.end(at: DicePosition(row: 4, col: 5))
+
+        XCTAssertNil(board.swappedDirection)
+    }
+
+    func testSwapSinisterForward() {
+        board.size = 5
+
+        sut.begin(at: DicePosition(row: 0, col: 4))
+        sut.end(at: DicePosition(row: 4, col: 0))
+
+        XCTAssertEqual(board.swappedDiagonal, .sinister)
+        XCTAssertEqual(board.swappedDirection, .forward)
+    }
+
+    func testSwapSinisterBackward() {
+        board.size = 5
+
+        sut.begin(at: DicePosition(row: 4, col: 0))
+        sut.end(at: DicePosition(row: 0, col: 4))
+
+        XCTAssertEqual(board.swappedDiagonal, .sinister)
+        XCTAssertEqual(board.swappedDirection, .backward)
+    }
+
+    func testDontSwapIfRandomMoves() {
         sut.begin(at: DicePosition(row: 1, col: 2))
         sut.end(at: DicePosition(row: 2, col: 5))
 
-        XCTAssertNil(board.swappedRow)
+        sut.begin(at: DicePosition(row: 1, col: 4))
+        sut.end(at: DicePosition(row: 2, col: 1))
+
+        XCTAssertNil(board.swappedDirection)
+    }
+}
+
+class DiceBoardMock: SwappableBoard {
+    var size: Int = .max
+    var swappedRow: Int?
+    var swappedCol: Int?
+    var swappedDiagonal: Diagonal?
+    var swappedDirection: DiceSwapDirection?
+
+    func swap(rowAt row: Int, direction: DiceSwapDirection) {
+        swappedRow = row
+        swappedDirection = direction
     }
 
-    func testDontSwapRowBackwardIfRowChanged() {
-        sut.begin(at: DicePosition(row: 1, col: 5))
-        sut.end(at: DicePosition(row: 2, col: 2))
+    func swap(colAt col: Int, direction: DiceSwapDirection) {
+        swappedCol = col
+        swappedDirection = direction
+    }
 
-        XCTAssertNil(board.swappedRow)
+    func swap(diagonal: Diagonal, direction: DiceSwapDirection) {
+        swappedDiagonal = diagonal
+        swappedDirection = direction
     }
 }
